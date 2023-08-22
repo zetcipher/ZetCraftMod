32130b9e26b776f0299aaffa8fb885bc0cdd1c99
ZetCipher <numbah90@gmail.com>
2023-08-21 20:33:44 -0400

    added soul sphere items

--- src/main/java/net/zetcipher/zetcraft/init/ModItems.java
+++ src/main/java/net/zetcipher/zetcraft/init/ModItems.java
@@ -20,6 +20,15 @@ public class ModItems {
     public static final RegistryObject<Item> STAR_PIECE = ITEMS.register("star_piece",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ZETCRAFT)));
 
+    public static final RegistryObject<Item> PLAYER_SOUL_I = ITEMS.register("plr_soul_i",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ZETCRAFT)));
+
+    public static final RegistryObject<Item> PLAYER_SOUL_II = ITEMS.register("plr_soul_ii",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ZETCRAFT)));
+
+    public static final RegistryObject<Item> PLAYER_SOUL_III = ITEMS.register("plr_soul_iii",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ZETCRAFT)));
+
     public static final RegistryObject<ThunderRageItem> THUNDER_RAGE = ITEMS.register("thunder_rage",
             () -> new ThunderRageItem(new Item.Properties().tab(ModCreativeModeTab.ZETCRAFT).stacksTo(1).rarity(Rarity.UNCOMMON)));
 
